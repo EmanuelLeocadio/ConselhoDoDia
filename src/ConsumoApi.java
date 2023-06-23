c640845decea084e36ddbe3ab3f5bd3cde79b756
EmanuelLeocadio <emanuelleocadioramalho@gmail.com>
2023-06-23 17:46:20 -0300

    first commit

---
+++ src/ConsumoApi.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsumoApi {
+    
+    public String NumAleatorio( String numero){
+  String conselho = "https://api.adviceslip.com/advice/" + numero;
+  HttpClient cliente = HttpClient.newHttpClient();
+  HttpRequest request = HttpRequest.newBuilder()
+       .uri(URI.create(conselho)).build();
+       HttpResponse<String> response;
+
+    try {
+        response = cliente.send(request, HttpResponse.BodyHandlers.ofString());
+        return response.body();
+
+
+    } catch (IOException | InterruptedException e) {
+     e.printStackTrace();
+    }
+
+
+
+
+return "";
+
+
+    }
+}
